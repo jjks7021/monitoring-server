68bd9ba96d3786ea9416b0abcaf41411c6cc5b31
jjks7021 <jjks7021@naver.com>
2026-05-12 15:33:57 +0900

    feat: 고독사 분석용 DB 및 엔티티 구조 변경

--- src/main/java/com/godoksa/monitoring/repository/DeviceRepository.java
+++ src/main/java/com/godoksa/monitoring/repository/DeviceRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+    // 스마트폰 고유 ID(UUID)로 기기 정보를 찾는 기능을 추가합니다.
     Optional<Device> findByHardwareId(String hardwareId);
 }
